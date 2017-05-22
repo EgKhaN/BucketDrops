4e0ac727e72c8c9fc2f1d015a3e842b7d37f6337
egkhan <Egka61git>
2017-05-23 00:35:59 +0300

    Ders 50 bitti

--- app/src/main/java/com/egkhan/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/egkhan/bucketdrops/ActivityMain.java
@@ -1,13 +1,30 @@
 package com.egkhan.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initBackgroundImage();
+
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.mipmap.background)
+                .into(background);
     }
 }
