03abb6d4d185568faed5d20343b9592d50698f17
egkhan <Egka61git>
2017-05-25 23:41:15 +0300

    Bölüm 9 bitti

--- app/src/main/java/com/egkhan/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/egkhan/bucketdrops/ActivityMain.java
@@ -3,6 +3,8 @@ package com.egkhan.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,6 +12,19 @@ import com.bumptech.glide.Glide;
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar toolbar;
+    Button btnAdd;
+    View.OnClickListener btnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        DialogAdd dialogAdd = new DialogAdd();
+        dialogAdd.show(getSupportFragmentManager(),"Add");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +32,8 @@ public class ActivityMain extends AppCompatActivity {
 
         toolbar =(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(btnAddListener);
         initBackgroundImage();
 
     }
